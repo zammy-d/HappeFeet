95aa9e4e1c4e427b76fe4dac6159375643087e86
zammy-d <73567026+zammy-d@users.noreply.github.com>
2023-06-28 11:32:59 -0500

    Update index.php
    Se actualiza botón agregar al carrito

--- index.php
+++ index.php
@@ -169,7 +169,7 @@ include 'components/wishlist_cart.php';
          <div class="price"><span>$</span><?= $fetch_product['price']; ?><span>/-</span></div>
          <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
       </div>
-      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+      <input type="submit" value="agregar al carrito" class="btn" name="add_to_cart">
    </form>
    <?php
       }
