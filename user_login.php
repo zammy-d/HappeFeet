940f5f23edc449b6d0bdb07971afc8e1cce00952
zammy-d <73567026+zammy-d@users.noreply.github.com>
2023-06-28 13:30:25 -0500

    Update user_login.php
    Se actualiza, redireccion a index.php

--- user_login.php
+++ user_login.php
@@ -23,7 +23,7 @@ if(isset($_POST['submit'])){
 
    if($select_user->rowCount() > 0){
       $_SESSION['user_id'] = $row['id'];
-      header('location:home.php');
+      header('location:index.php');
    }else{
       $message[] = '¡Nombre de usuario o contraseña incorrecta!';
    }
